2565e05377ec975634336d17487c3497992c6de6
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-06 23:11:13 +0600

    mongoconnect

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@inventory.axhes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("inventory").collection("products");
+    const orderCollection = client.db("inventory").collection("order");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/apple", (req, res) => {
   res.send([{ name: "Afridi" }, { name: "saad" }]);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
 app.listen(port, () => {
