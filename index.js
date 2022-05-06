a3244e920e6e4370230b17a14bb8165023a4ff70
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-06 22:11:43 +0600

    heroku

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+app.get("/apple", (req, res) => {
+  res.send([{ name: "Afridi" }, { name: "saad" }]);
+});
 
 app.listen(port, () => {
-  console.log("Listening");
+  console.log("Listening to server");
 });
